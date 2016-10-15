f69fe4af714e9158f668e59413d598c2daa1f0e0
zhang <zz593141477@gmail.com>
2016-10-15 14:25:29 +0800

    grade import & export

--- static/js/grades.js
+++ static/js/grades.js
@@ -1,4 +1,4 @@
-var gridApp = angular.module('app', ['ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.edit']);
+var gridApp = angular.module('app', ['ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.edit', 'ui.grid.importer']);
 
 gridApp.controller('MainCtrl', ['$scope', '$http', '$interval', function ($scope, $http, $interval) {
 
@@ -20,6 +20,83 @@ gridApp.controller('MainCtrl', ['$scope', '$http', '$interval', function ($scope
         alert("Error!!!");
     });
   };
+  $scope.action_export = function(){
+    function datenum(v, date1904) {
+      if(date1904) v+=1462;
+      var epoch = Date.parse(v);
+      return (epoch - new Date(Date.UTC(1899, 11, 30))) / (24 * 60 * 60 * 1000);
+    }
+     
+    function sheet_from_array_of_arrays(data, opts) {
+      var ws = {};
+      var range = {s: {c:10000000, r:10000000}, e: {c:0, r:0 }};
+      for(var R = 0; R != data.length; ++R) {
+        for(var C = 0; C != data[R].length; ++C) {
+          if(range.s.r > R) range.s.r = R;
+          if(range.s.c > C) range.s.c = C;
+          if(range.e.r < R) range.e.r = R;
+          if(range.e.c < C) range.e.c = C;
+          var cell = {v: data[R][C] };
+          if(cell.v == null) continue;
+          var cell_ref = XLSX.utils.encode_cell({c:C,r:R});
+          
+          if(typeof cell.v === 'number') cell.t = 'n';
+          else if(typeof cell.v === 'boolean') cell.t = 'b';
+          else if(cell.v instanceof Date) {
+            cell.t = 'n'; cell.z = XLSX.SSF._table[14];
+            cell.v = datenum(cell.v);
+          }
+          else cell.t = 's';
+          
+          ws[cell_ref] = cell;
+        }
+      }
+      if(range.s.c < 10000000) ws['!ref'] = XLSX.utils.encode_range(range);
+      return ws;
+    }
+     
+    /* original data */
+    var data = $scope.build_export_data();
+    var ws_name = "Sheet1";
+     
+    function Workbook() {
+      if(!(this instanceof Workbook)) return new Workbook();
+      this.SheetNames = [];
+      this.Sheets = {};
+    }
+     
+    var wb = new Workbook(), ws = sheet_from_array_of_arrays(data);
+     
+    /* add worksheet to workbook */
+    wb.SheetNames.push(ws_name);
+    wb.Sheets[ws_name] = ws;
+    var wbout = XLSX.write(wb, {bookType:'xlsx', bookSST:true, type: 'binary'});
+
+    function s2ab(s) {
+      var buf = new ArrayBuffer(s.length);
+      var view = new Uint8Array(buf);
+      for (var i=0; i!=s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+      return buf;
+    }
+    saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), "export.xlsx")
+  };
+  $scope.build_export_data = function(){
+    var result = [], tmp = [];
+    for (var i = 0; i < $scope.gridOptions.columnDefs.length; i++) {
+      var cdef = $scope.gridOptions.columnDefs[i];
+      tmp.push(cdef.displayName);
+    }
+    result.push(tmp);
+    $scope.data.forEach(function(row){
+      tmp = [];
+      for (var i = 0; i < $scope.gridOptions.columnDefs.length; i++) {
+        var cdef = $scope.gridOptions.columnDefs[i];
+        tmp.push((cdef.name in row) ? row[cdef.name] : '');
+      }
+      result.push(tmp);
+    });
+    return result;
+  };
 
   $scope.gridOptions = {
     enableCellEditOnFocus: true,
@@ -36,8 +113,41 @@ gridApp.controller('MainCtrl', ['$scope', '$http', '$interval', function ($scope
         action: $scope.action_submit,
         order: 410
       },
+      {
+        title: '导出',
+        action: $scope.action_export,
+        order: 420
+      },
     ],
     data: 'data',
+    importerDataAddCallback: function ( grid, newObjects ) {
+      var displayName2name = {};
+      $scope.gridOptions.columnDefs.forEach(function(value){
+        displayName2name[value.displayName] = value.name;
+      });
+      newObjects.forEach(function(row){
+        for(var prop in row){
+          if(displayName2name[prop]!==null)
+            row[displayName2name[prop]]=row[prop];
+        };
+        if(!('student_id' in row))return;
+        $scope.data.forEach(function(tr){
+          if(tr['student_id'] == row['student_id']){
+            try{
+              tr['grade']=parseFloat(row['grade'],10);
+              if(isNaN(tr['grade']))
+                throw 'Not a number';
+            }catch(err){
+              tr['grade']='';
+            }
+            if(tr['grade']>100)
+              tr['grade']=100;
+            else if(tr['grade']<0)
+              tr['grade']=0;
+          }
+        })
+      });
+    },
     onRegisterApi: function(gridApi){
       $scope.gridApi = gridApi;
       gridApi.edit.on.afterCellEdit($scope,function(rowEntity, colDef){
