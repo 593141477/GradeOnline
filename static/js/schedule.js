569ba41eb27ede8e667fe034202ddeb9ec0d3fa1
ZhangYuxiang <zz593141477@gmail.com>
2017-02-13 22:09:13 +0800

    schedule export

--- static/js/schedule.js
+++ static/js/schedule.js
@@ -24,7 +24,7 @@ for(i in teacher_list)
 {
     metaTeacherList[i] = {id:teacher_list[i]._id, teacher:teacher_list[i].name}
 }
-gridApp.controller('MainCtrl', ['$scope', '$http', '$interval', function ($scope, $http, $interval) {
+gridApp.controller('MainCtrl', ['$scope', '$http', '$interval', '$filter', function ($scope, $http, $interval, $filter) {
 
   $scope.dirty=-1;
   $scope.data = schedule;
@@ -39,6 +39,91 @@ gridApp.controller('MainCtrl', ['$scope', '$http', '$interval', function ($scope
     }, function (response) {
         alert("Error!!!");
     });
+  };
+  $scope.action_export = function(){
+    function datenum(v, date1904) {
+      if(date1904) v+=1462;
+      var epoch = Date.parse(v);
+      return (epoch - new Date(Date.UTC(1899, 11, 30))) / (24 * 60 * 60 * 1000);
+    }
+     
+    function sheet_from_array_of_arrays(data, opts) {
+      var ws = {};
+      var range = {s: {c:10000000, r:10000000}, e: {c:0, r:0 }};
+      for(var R = 0; R != data.length; ++R) {
+        for(var C = 0; C != data[R].length; ++C) {
+          if(range.s.r > R) range.s.r = R;
+          if(range.s.c > C) range.s.c = C;
+          if(range.e.r < R) range.e.r = R;
+          if(range.e.c < C) range.e.c = C;
+          var cell = {v: data[R][C] };
+          if(cell.v == null) continue;
+          var cell_ref = XLSX.utils.encode_cell({c:C,r:R});
+          
+          if(typeof cell.v === 'number') cell.t = 'n';
+          else if(typeof cell.v === 'boolean') cell.t = 'b';
+          else if(cell.v instanceof Date) {
+            cell.t = 'n'; cell.z = XLSX.SSF._table[14];
+            cell.v = datenum(cell.v);
+          }
+          else cell.t = 's';
+          
+          ws[cell_ref] = cell;
+        }
+      }
+      if(range.s.c < 10000000) ws['!ref'] = XLSX.utils.encode_range(range);
+      return ws;
+    }
+     
+    /* original data */
+    var data = $scope.build_export_data();
+    var ws_name = "Schedule";
+     
+    function Workbook() {
+      if(!(this instanceof Workbook)) return new Workbook();
+      this.SheetNames = [];
+      this.Sheets = {};
+    }
+     
+    var wb = new Workbook(), ws = sheet_from_array_of_arrays(data);
+     
+    /* add worksheet to workbook */
+    wb.SheetNames.push(ws_name);
+    wb.Sheets[ws_name] = ws;
+    var wbout = XLSX.write(wb, {bookType:'xlsx', bookSST:true, type: 'binary'});
+
+    function s2ab(s) {
+      var buf = new ArrayBuffer(s.length);
+      var view = new Uint8Array(buf);
+      for (var i=0; i!=s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+      return buf;
+    }
+    saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), "schedule.xlsx")
+  };
+  $scope.build_export_data = function(){
+    var result = [], tmp = [];
+    for (var i = 0; i < $scope.gridOptions.columnDefs.length; i++) {
+      var cdef = $scope.gridOptions.columnDefs[i];
+      if(cdef.name == 'DeleteOp') continue;
+      tmp.push(cdef.displayName);
+    }
+    result.push(tmp);
+    $scope.data.forEach(function(row){
+      tmp = [];
+      for (var i = 0; i < $scope.gridOptions.columnDefs.length; i++) {
+        var cdef = $scope.gridOptions.columnDefs[i];
+        if(cdef.name == 'DeleteOp') continue;
+        var filter = ('cellFilter' in cdef) ? $filter(cdef.cellFilter) : function(x){return x;};
+        var d = '';
+        try{
+          eval('d=row.'+cdef.name);
+          d = d || '';
+        }catch(exception) {}
+        tmp.push(filter(d));
+      }
+      result.push(tmp);
+    });
+    return result;
   };
   $scope.DeleteRow = function(row) {
     var index = $scope.data.indexOf(row.entity);
@@ -70,11 +155,16 @@ gridApp.controller('MainCtrl', ['$scope', '$http', '$interval', function ($scope
         order: 410
       },
       {
-        title: 'clean',
+        title: '导出',
+        action: $scope.action_export,
+        order: 420
+      },
+      {
+        title: '清空',
         action: function ($event) {
           $scope.data = [];
         },
-        order: 310
+        order: 330
       },
       {
         title: '增加',
